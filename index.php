1b6728300de582fa934bdc7af986e236c18b6576
Jordi Rivero <jrivero24@yahoo.es>
2011-06-27 23:18:19 +0200

    refactor

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 <?php
 
+error_reporting(E_ALL);
+
 require_once __DIR__.'/lib/silex/silex.phar';
 require_once __DIR__.'/lib/NotORM/NotORM.php';
-require_once __DIR__.'/lib/Haanga/Haanga.php';
+require_once __DIR__.'/lib/Haanga/lib/Haanga.php';
+
+use Silex\Application;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -10,8 +14,33 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 $app = new Silex\Application();
 
-$app->register(new Silex\Extension\SessionExtension());
-Haanga::configure(array('template_dir' => __DIR__.'/views/', 'cache_dir' => __DIR__.'/var/views'));
+$app['config'] = $app->share(function() use ($app) {
+	$config_file = __DIR__.'/config/application.json';
+    return json_decode(file_get_contents($config_file), true);
+});
+
+if (defined('ENABLE_LOGGING'))
+{
+	$this->register(new Silex\Extension\MonologExtension(), array(
+		'monolog.logfile' => __DIR__.'/var/log/dev.log',
+		'monolog.class_path' => __DIR__.'/lib/monolog/src',
+	));
+}
+
+if (defined('ENABLE_SESSION'))
+{
+	$app->register(new Silex\Extension\SessionExtension());
+}
+
+Haanga::configure(array(
+	'template_dir'=>__DIR__ . '/' . $app['config']['views']['folder'],
+	'cache_dir'=>__DIR__ . '/' . $app['config']['views']['cache'],
+	'debug'=>$app['config']['views']['debug'],
+));
+
+$app['database'] = $app->share(function() {
+    return new NotORM(new PDO('mysql:dbname=test', 'root'));
+});
 
 $app->get('/', function() {
 	return "Index Page";
@@ -29,18 +58,12 @@ $app->get('/hello/{name}', function ($name) {
     return "Hello $name";
 });
 
-$app->get('/test/db', function () {
-	$db = new NotORM(new PDO("mysql:dbname=software"));
+$app->get('/test/database', function() use ($app) {
+	$db = $app['database'];
 
-	$applications = $db->application()
-		->select("id, title")
-		->where("web LIKE ?", "http://%")
-		->order("title")
-		->limit(10);
-		
-	foreach ($applications as $id => $application) {
-		echo "$application[title]\n";
-	}
+    $entities = $db->entity()->limit(10);
+    
+	print_r($entities);
 });
 
 $app->get('/test/view', function () {
@@ -48,7 +71,7 @@ $app->get('/test/view', function () {
 	$url  = 'http://silex/account';
 	$cxt = compact('user', 'url');
 
-	return new Response(Haanga::Load('sample_template.html', $cxt));
+	return new Response(Haanga::Load('sample_template.html', $cxt, true));
 });
 
 $app->get('/login', function () use ($app) {
@@ -81,7 +104,7 @@ $app->error(function (\Exception $e) {
     }
 
     $code = ($e instanceof HttpException) ? $e->getStatusCode() : 500;
-    return new Response('We are sorry, but something went terribly wrong.', $code);
+    return new Response('We are sorry, but something went terribly wrong: ' . $e->getMessage(), $code);
 });
 
 $app->run();
